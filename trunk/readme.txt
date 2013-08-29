d6c087495346ceb4c44b7c0a22b894e1637741a7
ctala <ctala@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-08-29 15:27:39 +0000

    Change the version
    git-svn-id: https://plugins.svn.wordpress.org/woocommerce-chilean-peso-currency/tags/2.2@764271 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ trunk/readme.txt
@@ -0,0 +1,34 @@
+=== WooCommerce Chilean Peso  ===
+
+Contributors: Cristian Tala S.
+
+
+
+Tags: 
+
+Requires at least: 3, 3.3+ and WooCommerce 1.4+, Chilean Peso, Pesos Chilenos, currency
+
+Tested up to: 3.6
+
+Stable tag: 2.2
+
+== Description ==
+
+This Small Plugin add The Chilean Peso currency and symbol to woocommerce.
+Add the different states to Woocommerce.
+Enable the paypal payment through CLP to USD conversion.
+
+
+Lo que hace este plugin es facilitar la integración de los pesos Chilenos a Woocommerce, además de agregar las distintas regiones del país ( Chile obviamente ) y aceptar pagos con paypal a través de dolares. 
+
+
+
+== Changelog ==
+
+
+= 2.2 =
+Agregada la posibilidad de usar paypal con woocommerce y chilean pesos.
+
+= 2.0 =
+Added the Chilean states for WooCommerce
+Agregadas las Regiones de Chile a WooCommerce
---
+++ trunk/woocommerce-chilean-peso.php
@@ -0,0 +1,96 @@
+<?php
+
+/*
+  Plugin Name: WooCommerce Chilean Peso + Chilean States
+  Plugin URI: http://plugins.svn.wordpress.org/woocommerce-chilean-peso-currency/
+  Description: This plugin add the chilean currency, symbol , paypal and the states to WooCommerce.
+  Version: 2.2
+  Author: Cristian Tala Sánchez <cristian.tala@gmail.com>
+  Author URI: http://www.cristiantala.cl
+  License: GPLv3
+  Requires at least: 3.0 +
+  Tested up to: 3.6
+ */
+/*
+ *      Copyright 2012 Cristian Tala Sánchez <cristian.tala@gmail.com>
+ *
+ *      This program is free software; you can redistribute it and/or modify
+ *      it under the terms of the GNU General Public License as published by
+ *      the Free Software Foundation; either version 3 of the License, or
+ *      (at your option) any later version.
+ *
+ *      This program is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU General Public License for more details.
+ *
+ *      You should have received a copy of the GNU General Public License
+ *      along with this program; if not, write to the Free Software
+ *      Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ *      MA 02110-1301, USA.
+ */
+
+function add_clp_currency($currencies) {
+    $currencies["CLP"] = 'Pesos Chilenos';
+    return $currencies;
+}
+
+function add_clp_currency_symbol($currency_symbol, $currency) {
+    switch ($currency) {
+        case 'CLP': $currency_symbol = '$';
+            break;
+    }
+    return $currency_symbol;
+}
+
+function custom_woocommerce_states($states) {
+
+    $states['CL'] = array(
+        'I' => 'I de Tarapacá',
+        'II' => 'II de Antofagasta',
+        'III' => 'III de Atacama',
+        'IV' => 'IV de Coquimbo',
+        'V' => 'V de Valparaíso',
+        'VI' => 'VI del Libertador General Bernardo O\'Higgins',
+        'VII' => 'VII del Maule',
+        'VIII' => 'VIII del Bío Bío',
+        'IX' => 'IX de la Araucanía',
+        'XIV' => 'XIV de los Ríos',
+        'X' => 'X de los Lagos',
+        'XI' => 'XI Aysén del General Carlos Ibáñez del Campo',
+        'XII' => 'XII de Magallanes y Antártica Chilena',
+        'RM' => 'Metropolitana de Santiago',
+        'XV' => 'XV de Arica y Parinacota',
+    );
+
+    return $states;
+}
+
+
+    function add_clp_paypal_valid_currency( $currencies ) {  
+     array_push ( $currencies , 'CLP' );
+     return $currencies;  
+    } 
+
+
+function convert_clp_to_usd($paypal_args){
+	if ( $paypal_args['currency_code'] == 'CLP'){
+		$convert_rate = 520; //set the converting rate
+		$paypal_args['currency_code'] = 'USD'; //change CLP to USD
+		$i = 1;
+
+		while (isset($paypal_args['amount_' . $i])) {
+			$paypal_args['amount_' . $i] = round( $paypal_args['amount_' . $i] / $convert_rate, 2);
+			++$i;
+		}
+
+	}
+return $paypal_args;
+}
+
+add_filter('woocommerce_paypal_args', 'convert_clp_to_usd');
+add_filter('woocommerce_states', 'custom_woocommerce_states');
+add_filter('woocommerce_currencies', 'add_clp_currency', 10, 1);
+add_filter('woocommerce_currency_symbol', 'add_clp_currency_symbol', 10, 2);
+add_filter( 'woocommerce_paypal_supported_currencies', 'add_clp_paypal_valid_currency' );     
+?>
